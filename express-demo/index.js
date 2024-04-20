6e04ef389aff1b4e16c21393d7ddc4e93ec43f3a
huzain <holayinka@rocketmail.com>
2024-04-20 13:28:12 +0100

    Node course

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,46 +1,82 @@
 const express = require('express');
-
+const Joi = require('joi');
+const { error } = require('joi/lib/types/lazy');
+const log = require('./logger');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
-app.use(express.json);
+const config = require('config');
+
+console.log(`the value of the env is ${process.env.NODE_ENV}`);
+console.log(app.get('env'));
+
+console.log(`application password is ${config.get('mail.password')}`);
+
+//confuguration 
+console.log(`Application Name: ${config.get('name')}`);
+console.log(`Mail Server: ${config.get('mail.host')}`);
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'))
+app.use(log);
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    console.log('Authenticating...');
+    next();
+})
+
+
 
 const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-    {id: 4, name: 'course4'},
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' },
+
 ];
-app.get('/', (req, res) => {    
+
+app.get('/', (req, res) => {
     res.send('Hello World');
-    res.end;
 }
 );
 
 app.get('/api/courses', (req, res) => {
     console.log(`inside the value ${req}`);
     res.send(courses);
-    res.end;
+
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    console.log(`inside the value ${req}`);
-    const course = courses.find(p=> p.id === parseInt(req.params.id));
-    if(!course) res.status(404).send("Course with the given id is not found");
+    const schema ={
+        name: Joi.string().min(3).required()
+    }
+    const result = Joi.validate(req.body, schema);
+    console.log(`the value of the result from validation is ${result}`);
+    if (!req.body.name || req.body.name.length < 3) res.status(400).send("Name is required and should be minimum 3 characters");
+    const course = courses.find(p => p.id === parseInt(req.params.id));
+      if (!course) res.status(404).send("Course with the given id is not found");
     res.send(course);
-    res.end;
+
 })
 
-app.post('/api/courses', (req,res)=>{
-    console.log(`inside the value ${req}`);
-    const course ={
+app.post('/api/courses', (req, res) => {
+    const schema ={
+        name: Joi.string().min(3).required()
+    }
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+    if (result.error) res.status(400).send(result.error.details[0].message);
+       const course = {
         id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
-    
+
     res.send(course);
-    res.end;
+
 })
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log(`listening on ${port}.........`))
+app.listen(port, () => console.log(`listening on ${port}.........`))
